0f850cbf6ab1a36ba673347915156dbce224c335
Victor Lezcano <victormaverik@gmail.com>
2022-11-25 20:58:03 -0300

    Version revisada Sockets

---
+++ public/js/main.js
@@ -0,0 +1,42 @@
+const socket = io();
+
+const formAgregarProducto = document.getElementById('formAgregarProducto')
+formAgregarProducto.addEventListener('submit', e => {
+    // prevengo que el formulario recargue la pagina al hacer submit
+    e.preventDefault()
+
+    // armo la persona extrayendo los datos de los campos del formulario
+
+    const producto = {
+        producto: formAgregarProducto[0].value, // document.getElementById('txtProducto').value
+        URL: formAgregarProducto[1].value, // document.getElementById('txtUrl').value
+    }
+
+    // envio la persona al servidor via socket
+    socket.emit('update', producto);
+
+    // limpio el contenido de los campos del formulario
+    formAgregarProducto.reset()
+})
+
+// agrego manejador de eventos de tipo 'personas'
+socket.on('producto', manejarEventoProducto);
+
+async function manejarEventoProducto(producto) {
+    // console.log(producto)
+
+    // busco la plantilla del servidor
+    const recursoRemoto = await fetch('plantillas/tabla-productos.hbs')
+
+    //extraigo el texto de la respuesta del servidor
+    const textoPlantilla = await recursoRemoto.text()
+
+    //armo el template con handlebars
+    const functionTemplate = Handlebars.compile(textoPlantilla)
+
+    // relleno la plantilla con las productos recibidas
+    const html = functionTemplate({ producto })
+
+    // reemplazo el contenido del navegador con los nuevos datos
+    document.getElementById('productos').innerHTML = html
+}
